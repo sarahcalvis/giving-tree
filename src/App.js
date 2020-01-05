9e30f570be33ddd7f482ff10e3941d6b91052571
sarahcalvis <sarahcalvis@gmail.com>
2020-01-04 22:34:47 -0500

    missed committing this file in last commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import firebase from './firebase.js';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import theme from './theme';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      testMessage: "test failed",
+    }
+  }
+  componentDidMount() {
+    var db = firebase.firestore();
+    var dbRef = db.collection("communityFoundations");
+    var testMessage = "";
+
+    dbRef.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        testMessage = doc.data().test;
+        console.log(testMessage);
+      });
+
+      this.setState({ testMessage: testMessage})
+
+      if (this.state.loaded === false) {
+        this.observeTestMessage();
+        this.setState({ loaded: true })
+      }
+    });
+  }
+
   render() {
     return (
       <div>
         <BrowserRouter>
           <ThemeProvider theme={theme}>
             <CssBaseline />
+            <p>{this.state.testMessage}</p>
           </ThemeProvider>
         </BrowserRouter>
       </div>
