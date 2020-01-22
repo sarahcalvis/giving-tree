a4691153f38fc75e305a9b59a50234ca987a0e2b
sarahcalvis <sarahcalvis@gmail.com>
2020-01-21 20:36:45 -0500

    Converted App.js to a function

--- src/App.js
+++ src/App.js
@@ -1,49 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import firebase from './firebase.js';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import theme from './theme';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      testMessage: "test failed",
-    }
-  }
-  componentDidMount() {
+export default function App() {
+  const [testMessage, setTestMessage] = useState('test failed');
+
+  useEffect(() => {
     var db = firebase.firestore();
     var dbRef = db.collection("communityFoundations");
-    var testMessage = "";
+    var newMessage = "";
 
     dbRef.get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-        testMessage = doc.data().test;
-        console.log(testMessage);
+        newMessage = doc.data().test;
+        console.log(newMessage);
       });
 
-      this.setState({ testMessage: testMessage})
+      setTestMessage(newMessage);
 
-      if (this.state.loaded === false) {
-        this.observeTestMessage();
-        this.setState({ loaded: true })
-      }
+      // if (this.state.loaded === false) {
+      //   this.observeTestMessage();
+      //   this.setState({ loaded: true })
+      // }
     });
-  }
+  })
 
-  render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <p>{this.state.testMessage}</p>
-          </ThemeProvider>
-        </BrowserRouter>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <p>{testMessage}</p>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
-
-export default App;
