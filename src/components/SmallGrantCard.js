817e588d6bfb6a7289e39c7511a5b6c1d8da9151
Calvis <CALVISSE1@GCC.EDU>
2020-01-23 20:58:04 -0500

    Images on SmallGrantCard

--- src/components/SmallGrantCard.js
+++ src/components/SmallGrantCard.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
+import firebase from '../firebase.js';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import { Link } from 'react-router-dom';
@@ -33,6 +34,8 @@ export default function SmallGrantCard(props) {
   const [nonprofit, setNonprofit] = React.useState(props.nonprofit);
   const [goal, setGoal] = React.useState(props.goal);
   const [raised, setRaised] = React.useState(props.raised);
+  const [image, setImage] = React.useState('');
+  const [url, setUrl] = React.useState('');
 
   // Observe grant details
   useEffect(() => { setGrant(props.grant); }, [props.grant]);
@@ -41,16 +44,35 @@ export default function SmallGrantCard(props) {
   useEffect(() => { setGoal(props.goal); }, [props.goal]);
   useEffect(() => { setRaised(props.raised); }, [props.raised]);
 
+  // Get a reference to the storage service, which is used to create references in your storage bucket
+  let storage = firebase.storage();
+
+  // Create a storage reference from our storage service
+  let storageRef = storage.ref();
+
+
+  useEffect(() => {
+    // Create a child reference
+    // console.log('hi' +JSON.stringify(storageRef.child('GivingTree.png').getDownloadURL()))
+    storageRef.child('GivingTree.png').getDownloadURL().then((u) => {
+      setUrl(u)
+    }).catch( (error) => { 
+      console.log(error);
+    });
+  }, []);
+
+
+
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card className={classes.card}>
         <CardActionArea component={Link} to={'/' + grant.split(' ').join('-')}>
-          {/* <CardMedia
-          className={classes.cardMedia}
-          image={pictures[this.state.execPosition]}
-          title="Show Image"
-        /> */}
-          <Text type='card-heading' text='Image here' />
+          <CardMedia
+            className={classes.cardMedia}
+            image={url}
+            title="Grant Image"
+          />
+          {/* <img src={url} alt='no image'></img> */}
           <CardContent className={classes.cardContent}>
             <Text type='card-aboveheading' text={nonprofit} />
             <Text type='card-heading' text={grant} />
