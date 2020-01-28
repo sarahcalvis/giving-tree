641fafbb65c0112c93bd483446442b05ab60efcd
Calvis <CALVISSE1@GCC.EDU>
2020-01-27 22:53:29 -0500

    single quotes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Stripe Server
 // TODO: host this on firebase maybe?
 
-const app = require("express")();
-const stripe = require("stripe")("sk_test_avCwuzIvg45JxkjItTyqRGH600JEoryzyP");
+const app = require('express')();
+const stripe = require('stripe')('sk_test_avCwuzIvg45JxkjItTyqRGH600JEoryzyP');
 
-app.use(require("body-parser").text());
+app.use(require('body-parser').text());
 
-app.post("/charge", async (req, res) => {
+app.post('/charge', async (req, res) => {
   console.log(req.body);
   let source = req.body.split(' amount: ')[0];
   let amount = req.body.split(' amount: ')[1].split(' description: ')[0];
@@ -14,7 +14,7 @@ app.post("/charge", async (req, res) => {
   try {
     let {status} = await stripe.charges.create({
       amount:  amount,
-      currency: "usd",
+      currency: 'usd',
       description: description,
       source: source,
     });
@@ -26,4 +26,4 @@ app.post("/charge", async (req, res) => {
   }
 });
 
-app.listen(9000, () => console.log("Listening on port 9000"));
+app.listen(9000, () => console.log('Listening on port 9000'));
