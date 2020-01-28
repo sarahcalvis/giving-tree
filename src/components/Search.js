2818f12cc49ed8ee4b313d3b586cb58bcc730a35
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2020-01-28 12:04:02 -0500

    maps autocomplete works now

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import LocationSearch from './LocationSearch';
+import AutoCompleteMapsSearchBar from "./AutoCompleteMapsSearchBar";
+import { makeStyles } from "@material-ui/styles";
+
+
+const useStyles = makeStyles(theme => ({
+  searchbox: {
+    height: "100px"
+  }
+}));
 
 export default function Search() {
+  const classes = useStyles();
   return (
-      <LocationSearch/>
+      <div className={classes.searchbox}>
+        <AutoCompleteMapsSearchBar/>
+        <LocationSearch/>
+      </div>
+      
   );
 }
