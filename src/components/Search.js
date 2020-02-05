c694587a68f59e151c0a3d1517237a139dda831f
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2020-02-04 22:33:37 -0500

    corrected calcDist

--- src/components/Search.js
+++ src/components/Search.js
@@ -58,36 +58,37 @@ class Search extends Component {
   }
 
   //from Geo Data Source
-  calcDistance(lat1, lon1, lat2, lon2, unit) {
-    if (lat1 === lat2 && lon1 === lon2) {
+  calcDistance = (lat1, lon1, lat2, lon2) => {
+    if ((lat1 === lat2) && (lon1 === lon2)) {
       return 0;
-    } else {
-      var radlat1 = (Math.PI * lat1) / 180;
-      var radlat2 = (Math.PI * lat2) / 180;
-      var theta = lon1 - lon2;
-      var radtheta = (Math.PI * theta) / 180;
-      var dist =
-        Math.sin(radlat1) * Math.sin(radlat2) +
-        Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    }
+    else {
+      var radlat1 = Math.PI * lat1/180;
+      var radlat2 = Math.PI * lat2/180;
+      var theta = lon1-lon2;
+      var radtheta = Math.PI * theta/180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
       if (dist > 1) {
         dist = 1;
       }
       dist = Math.acos(dist);
-      dist = (dist * 180 ) / Math.PI;
+      dist = dist * 180/Math.PI;
       dist = dist * 60 * 1.1515;
       return dist;
     }
   }
 
   addDist = (grant) => {
+    let newDist = this.calcDistance(this.state.centerLoc.lat, this.state.centerLoc.long, grant.grant.lat, grant.grant.long);
     this.setState({
-      "dist" : this.calcDistance(this.state.centerLoc.lat, this.state.centerLoc.long, grant.lat, grant.long),
+      dist : newDist
     });
   }
 
   setDists = () => { 
     this.state.metaDocs.forEach(this.addDist); 
     var sortedByDist = this.state.metaDocs;
+    console.log("unsorted with dists: ", sortedByDist);
     sortedByDist.sort((a, b) => (a.dist > b.dist ? 1 : -1));
     this.setState({metaDocs: sortedByDist});
     this.setState({tempResults: sortedByDist});
