4eac8e9e1f2969bc0c84cd81b955935dbb9e2c53
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2020-02-24 20:31:29 -0500

    working warning modal in meta-admin page

--- src/components/Foundation.js
+++ src/components/Foundation.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import firebase from "../firebase.js";
 import WarningModal from './WarningModal.js';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
 
 
 const useStyles = makeStyles(theme => ({
@@ -22,20 +20,6 @@ const useStyles = makeStyles(theme => ({
     marginTop: '2em',
     maxWidth: '90%',
   },
-  media: {
-    height: '70px',
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
   bullet: {
     display: 'inline-block',
     margin: '0 2px',
@@ -45,12 +29,6 @@ const useStyles = makeStyles(theme => ({
     paddingLeft: '4em',
     paddingRight: '4em',
     marginRight: '1em',
-  },
-  root: {
-    flexGrow: 1,
-  },
-  dates: {
-    textAlign: 'right',
   },
   modal: {
     display: "flex",
@@ -64,141 +42,65 @@ const useStyles = makeStyles(theme => ({
     justifyContent: "end",
     border: "none",
   },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3)
-  },
-  tBox: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: 275
-    }
-  }
 }));
 
 export default function Foundation(props) {
   const classes = useStyles();
   const bull = <span className={classes.bullet}>•</span>;
-
-  return (
-    <div style = {{display: (props.status === "requested" ? "inline" : "none")}}>
-    <Card className={classes.card}>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-      </CardContent>  
-      <CardActions disableSpacing>
-        <Button variant="outlined" color="secondary" onClick={console.log("Approve")} className={classes.button}>
-          Approve
-        </Button>
-        <Button variant="outlined" color="primary" onClick={console.log("Deny")} className={classes.button}>
-          Deny
-        </Button>
-      </CardActions>
-      <CardContent className={classes.cardContent}>
-        <WarningModal
-            open={()=>{console.log("OPEN!")}}
-            handleClose={()=>{console.log("Close!!!")}}
-            handleYes={()=>{console.log("Yes!")}}
-        ></WarningModal>
-          
-      </CardContent>
-    </Card>
-    </div>
-  );
-  /*
   const [open, setOpen] = React.useState(false);
-  const [expanded, setExpanded] = React.useState(false);
 
-  var name = props.name;
-  var time = props.time;
-  var dj = props.dj;
-  var email = props.email;
-  var phone = props.phone;
-  var date = props.date;
-  var status = props.status;
-
-  function deleteDJ() {
-    var db = firebase.firestore();
-    console.log("foundation name is: ", props.name);
-    db.collection("foundations").doc(props.name).delete().then(function() {
-        console.log("Document successfully deleted!");
-    }).catch(function(error) {
-        console.error("Error removing document: ", error);
-    });
-  };
-
-  function approveDJ() {
-    var db = firebase.firestore();
-    
-    db.collection("foundations").doc(props.name).update({
-        status: 'current'
-    })
-    .then(function() {
-        console.log("Document successfully updated!");
-    })
-    .catch(function(error) {
-        // The document probably doesn't exist.
-        console.error("Error updating document: ", error);
-    });
-  };
 
   const handleOpen = () => {
+    console.log("handling the open");
     setOpen(true);
   };
 
   const handleYes = () => {
-    deleteDJ();
+    console.log("handling the yes");
     setOpen(false);
   };
 
   const handleClose = () => {
+    console.log("handling the close");
     setOpen(false);
   };
 
   const handleApprove = () => {
-    approveDJ();
+    console.log("handling approve");
   };
 
   return (
-    <div style = {{display: (props.status === "requested" ? "inline" : "none")}}>
-    <Card className={classes.card}>
-      <CardHeader title={name} />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          DJ: {dj}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          Requested Timeslot: {time}
-        </Typography>
-        
-        <Typography variant="body2" color="textSecondary" component="p">
-          Email: {email}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p" id="requestedTimeslot">
-          Phone Number: {phone}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <Button variant="outlined" color="secondary" onClick={handleApprove} className={classes.button}>
-          Approve
+    <div>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardContent className={classes.cardContent}>
+            <div>
+              <Typography gutterBottom variant="h5" component="h2">
+                Woot Woot
+            </Typography>
+              <Typography variant="h5" component="h2">
+                be{bull}nev{bull}o{bull}lent
+            </Typography>
+            </div>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button variant="outlined" color="secondary" onClick={handleApprove} className={classes.button}>
+            Approve
         </Button>
-        <Button variant="outlined" color="primary" onClick={handleOpen} className={classes.button}>
-          Deny
+          <Button variant="outlined" color="primary" onClick={handleOpen} className={classes.button}>
+            Deny
         </Button>
-      </CardActions>
-      <CardContent className={classes.cardContent}>
-        <WarningModal
+        </CardActions>
+        <CardContent className={classes.cardContent}>
+          <WarningModal
             open={open}
             handleClose={handleClose}
             handleYes={handleYes}
-        ></WarningModal>
-          
-      </CardContent>
-    </Card>
+          ></WarningModal>
+
+        </CardContent>
+      </Card>
     </div>
   );
-  */
 }
--- src/screens/MetaAdmin.js
+++ src/screens/MetaAdmin.js
@@ -5,7 +5,6 @@ import Foundation from '../components/Foundation.js';
 export default function MetaAdmin() {
   return(
     <div>
-      <p>Congrats you are the meta-admin</p>
       <Foundation></Foundation>
     </div>
     
