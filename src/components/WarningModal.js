9dd30093a2a36a6c6efedb22bd69e67b7b9577a0
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2020-02-25 17:56:06 -0500

    ok I'll come back later

--- src/components/WarningModal.js
+++ src/components/WarningModal.js
@@ -5,12 +5,16 @@ import Fade from "@material-ui/core/Fade";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles(theme => ({
+    buttonsWrapper: {
+      paddingTop: '2em',
+    },
     button: {
       paddingLeft: '4em',
       paddingRight: '4em',
@@ -33,25 +37,13 @@ const useStyles = makeStyles(theme => ({
 export default function WarningModal(props) {
     const classes = useStyles();
     const [cfInput, setCfInput] = React.useState(null);
-    const [isValid, setIsValid] = React.useState("false");
+    const [isValid, setIsValid] = React.useState("true");
     const handleInput = (e) => {
-      console.log("input data: ", e.target.value);
       setCfInput(e.target.value);      
     }
 
     useEffect(() => {
-      console.log("in use effect with cfInput === props.cfName: ", (cfInput === props.cfName));
-      console.log("in use effect with cfInput", cfInput);
-      console.log("in use effect with props.cfName: ", props.cfName);
-      setIsValid(cfInput === props.cfName);
-      /*
-      if(cfInput === props.cfName) {
-        setIsValid("true");
-      }
-      else {
-        setIsValid("false");
-      }
-*/
+      setIsValid(!(cfInput === props.cfName));
     }, [cfInput, props.cfName]);
 
     return(
@@ -68,31 +60,42 @@ export default function WarningModal(props) {
             >
             <Fade in={props.open}>
             <div className={classes.paper}>
-                <CardHeader title="WARNING" />
-                <Typography id="simple-modal-description">
-                Are you sure? This action can't be undone! Type "{props.cfName}" to confirm. 
-                </Typography>
-                <TextField 
-                  id='cf-name-field'
-                  fullWidth 
-                  label='Community Foundation Name'
-                  onChange={handleInput} />
-                <Button
-                variant="outlined"
-                color="primary"
-                type="submit"
-                disabled={!isValid}
-                className={classes.button}
-                onClick={props.handleYes}>
-                YES
-                </Button>
-                <Button
-                variant="outlined"
-                color="secondary"
-                className={classes.button}
-                onClick={props.handleClose}>
-                NO
-                </Button>
+                <CardHeader 
+                  titleTypographyProps={{variant:'h2', fontWeight:'bold' }}
+                  title="WARNING" />
+                <CardContent>
+                  <div>
+                    <Typography variant="body1" gutterBottom>
+                      Are you sure? This action can't be undone! 
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                      Type "{props.cfName}" to confirm. 
+                    </Typography>
+                    <TextField 
+                      id='cf-name-field'
+                      fullWidth 
+                      label='Community Foundation Name'
+                      onChange={handleInput} />   
+                    <div className={classes.buttonsWrapper}>
+                        <Button
+                        variant="outlined"
+                        color="primary"
+                        type="submit"
+                        disabled={isValid}
+                        className={classes.button}
+                        onClick={props.handleYes}>
+                        YES
+                      </Button>
+                      <Button
+                        variant="outlined"
+                        color="secondary"
+                        className={classes.button}
+                        onClick={props.handleClose}>
+                        NO
+                      </Button>
+                    </div>             
+                  </div>
+                </CardContent>
             </div>
             </Fade>
         </Modal>
