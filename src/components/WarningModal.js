fb5167f8561d80f5ba8696bbfb402b2e23c733f4
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2020-02-26 11:18:53 -0500

    Update WarningModal.js

--- src/components/WarningModal.js
+++ src/components/WarningModal.js
@@ -1,12 +1,13 @@
 
 import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Fade from "@material-ui/core/Fade";
-import Modal from "@material-ui/core/Modal";
+import Grid from "@material-ui/core/Grid";
 import Dialog from "@material-ui/core/Dialog";
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Backdrop from "@material-ui/core/Backdrop";
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -25,10 +26,15 @@ const useStyles = makeStyles(theme => ({
     header: {
       backgroundColor: theme.palette.warning,
       justifyContent:'center',
-      display: 'block',
+      display: 'flex',
+      verticalAlign: 'center',
+      direction: 'row',
+      justify: 'space-between',
+      alignItems: 'flex',
+      spacing: '{1}',
     },
-    icon: {
-      verticalAlign: 'baseline',
+    item: {
+      verticalAlign: 'center',
     },
     modal: {
       maxWidth: 'xs',
@@ -64,54 +70,60 @@ export default function WarningModal(props) {
             open={props.open}
             className={classes.modal}
             onClose={props.handleClose}
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-            timeout: 500
-            }}
             >
-            <Fade in={props.open}>
-            <div className={classes.paper}>
-                
-                  <div>
-                    <div className={classes.header}>
-                      <Typography variant="h6" gutterBottom>
-                        {<WarningRounded className={classes.icon}/>} Delete this Community Foundation? 
-                      </Typography>
-                      <Typography variant="body1" gutterBottom>
-                        Doing so will permanently delete its data, including all its giving opportunities. 
-                      </Typography>
-                    </div>
+            <Grid container>
+              <Grid item>
+                <DialogTitle >
+                  <Grid container className={classes.header} direction="row" justify='space-between' alignItems='flex' spacing={1}>
+                    <Grid item className={classes.item}>
+                      {<WarningRounded className={classes.icon}/>} 
+                    </Grid>
+                    <Grid item className={classes.item}>
+                        Delete this Community Foundation? 
+                    </Grid>
                     
-                    <Typography variant="body1" gutterBottom>
-                      Confirm you want to delete this community foundation by typing: {props.cfName} 
-                    </Typography>
-                    <TextField 
-                      id='cf-name-field'
-                      fullWidth 
-                      label='Community Foundation Name'
-                      onChange={handleInput} />   
-                    <div className={classes.buttonsWrapper}>
-                        <Button
-                        variant="outlined"
-                        color="primary"
-                        type="submit"
-                        disabled={isValid}
-                        className={classes.button}
-                        onClick={props.handleYes}>
-                        YES
-                      </Button>
-                      <Button
-                        variant="outlined"
-                        color="secondary"
-                        className={classes.button}
-                        onClick={props.handleClose}>
-                        NO
-                      </Button>
-                    </div>             
-                  </div>
+                  </Grid>
+                </DialogTitle>   
+              </Grid>
+              <Grid item>
                 
-            </div>
-            </Fade>
+              </Grid>
+              <Grid item>
+                <DialogContent>
+                  <DialogContentText>
+                    Doing so will permanently delete its data, including all its giving opportunities. 
+                  </DialogContentText>
+                  <DialogContentText>
+                    Confirm you want to delete this community foundation by typing: {props.cfName} 
+                  </DialogContentText>
+                  <TextField 
+                    id='cf-name-field'
+                    fullWidth 
+                    label='Community Foundation Name'
+                    onChange={handleInput} />
+                </DialogContent>  
+                <DialogActions>
+                  <div className={classes.buttonsWrapper}>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      type="submit"
+                      disabled={isValid}
+                      className={classes.button}
+                      onClick={props.handleYes}>
+                      YES
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      className={classes.button}
+                      onClick={props.handleClose}>
+                      NO
+                    </Button>
+                  </div>             
+                </DialogActions>
+              </Grid>
+            </Grid>
         </Dialog>
     );
 }
