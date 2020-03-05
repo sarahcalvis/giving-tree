bb6103754ae4886f9f452c70b0b6d4e5c9e660e5
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2020-03-05 15:43:44 -0500

    maybe it works?

--- src/screens/FDashboard.js
+++ src/screens/FDashboard.js
@@ -7,6 +7,8 @@ import firebase from '../firebase.js';
 import Search from '../components/Search';
 import { makeStyles } from '@material-ui/core/styles';
 import UserAuthContext from '../auth/context.js';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -17,6 +19,34 @@ const useStyles = makeStyles(theme => ({
 export default function FDashboard(props) {
   // List of small grant cards
   const [grants, setGrants] = React.useState([]);
+  const [expiredGrants, setExpiredGrants] = React.useState([]);
+  const [currentGrants, setCurrentGrants] = React.useState([]);
+  const [draftedGrants, setDraftedGrants] = React.useState([]);
+  const [shownGrants, setShownGrants] = React.useState([]);
+  const [status, setStatus] = React.useState('current');
+
+  const handleChange = (event, newStatus) => {
+    setStatus(newStatus);
+    if(newStatus === 'current') {
+        setShownGrants(currentGrants);
+    } else if(newStatus === 'drafted') {
+        setShownGrants(draftedGrants);
+    } else {
+        setShownGrants(expiredGrants);
+    }
+  };
+
+  const children = [
+    <ToggleButton key={1} value="current">
+      Current
+    </ToggleButton>,
+    <ToggleButton key={2} value="drafted">
+      Drafted
+    </ToggleButton>,
+    <ToggleButton key={3} value="expired">
+      Expired
+    </ToggleButton>
+  ];
   // Foundation ID
   const [id] = React.useState(props.id);
   //const user = withContext(UserAuthContext);
@@ -28,7 +58,9 @@ export default function FDashboard(props) {
   useEffect(() => {
     var newGrants = [];
     var newDocs = [];
-
+    var curGrants = [];
+    var exGrants = [];
+    var drGrants = [];
     if (!loading && !error) {
       snapshot.forEach(function (doc) {
         //console.log("cf_id, id: ", doc.data().cf_id, user.id);
@@ -64,14 +96,53 @@ export default function FDashboard(props) {
                         goalAmt={doc.data().goal_amt}
                         moneyRaised={doc.data().money_raised}
                         img={doc.data().images[0] || 'GivingTree.png'} />;
+            if(doc.data().status === 'current') {
+                curGrants.push(grant);
+            } else if (doc.data().status === 'drafted') {
+                drGrants.push(grant);
+            } else if (doc.data().status === 'expired') {
+                exGrants.push(grant);
+            }
             newGrants.push(grant);
         }
       });
       setGrants(newGrants);
+      setCurrentGrants(curGrants);
+      setDraftedGrants(drGrants);
+      setExpiredGrants(exGrants);
+      setShownGrants(curGrants);
       console.log("newDocs: ", newDocs);
       setDocs(newDocs);
     }
   }, [snapshot, error, loading]);
+
+  useEffect(() => {
+    var newGrants = [];
+    var curGrants = [];
+    var exGrants = [];
+    var drGrants = [];
+
+    docs.forEach((doc)=> {
+        let grant = <SmallGrantCard
+            id={doc.id}
+            title={doc.data().title}
+            cfName={doc.data().cf_name}
+            nonprofitName={doc.data().nonprofit_name}
+            goalAmt={doc.data().goal_amt}
+            moneyRaised={doc.data().money_raised}
+            img={doc.data().images[0] || 'GivingTree.png'} />;
+        if(doc.data().status === 'current') {
+            curGrants.push(grant);
+        } else if (doc.data().status === 'drafted') {
+            drGrants.push(grant);
+        } else if (doc.data().status === 'expired') {
+            exGrants.push(grant);
+        }
+    });
+      setCurrentGrants(curGrants);
+      setDraftedGrants(drGrants);
+      setExpiredGrants(exGrants);
+  }, [docs, grants]);
 
   function searchCallback(childData) {
     console.log("childData in dashboard: ", childData);
@@ -98,9 +169,16 @@ export default function FDashboard(props) {
     <Container maxWidth='md' className={classes.container}>
       {docs &&
         <div>
+          <Grid container spacing={2} direction="column" alignItems="center">
+            <Grid item>
+                <ToggleButtonGroup size="small" value={status} exclusive onChange={handleChange}>
+                {children}
+                </ToggleButtonGroup>
+            </Grid>
+          </Grid>
           <Search docs={docs} parentCallback={searchCallback} />
           <Grid container spacing={2} >
-            {grants}
+            {shownGrants}
           </Grid>
         </div>
       }
