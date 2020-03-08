020be69a1c0be21638ec52c8a757a0c0d781c0c0
Stahl4121 <Stahl4121@users.noreply.github.com>
2020-03-07 21:55:51 -0500

    customclaims admin script

--- server/script.js
+++ server/script.js
@@ -8,53 +8,69 @@ admin.initializeApp({
 });
 
 //------------------------------------------------------------
+//Script to set admin
+//------------------------------------------------------------
+
+// const uid = 'CFmOxVx6R8OWBMnZQoCfWhRAAEM2';
+
+// admin.auth().setCustomUserClaims(uid, { admin: true })
+// .then(() => {
+//   console.log('Set admin for ' + uid);
+// })
+// .catch((error) => {
+//   console.log('failed cc!');
+// });
+
+
+
+//------------------------------------------------------------
 //Script to add status and cfId to accounts
 //Add the uids into this array and then run this file
 //------------------------------------------------------------
 
-const uids = ['QJbnn1tU1EUCnllEGBAW4ZuLMG42', 'fn7ctzdQSRaDnSy3sd3qtnoHOnH2', 'nGhCVWTti7RFjVxOjbjwNgjxkWi1', 'raVpBAbRpXUscVkcxC3KXo1KZm22'];
+// const uids = ['QJbnn1tU1EUCnllEGBAW4ZuLMG42', 'fn7ctzdQSRaDnSy3sd3qtnoHOnH2', 'nGhCVWTti7RFjVxOjbjwNgjxkWi1', 'raVpBAbRpXUscVkcxC3KXo1KZm22'];
 
-uids.forEach((uid) => {
-  admin.auth().getUser(uid)
-    .then((user) => {
-      admin.firestore().collection('communityFoundations').where('personal_email', '==', user.email)
-        .get()
-        .then(function (querySnapshot) {
-          querySnapshot.forEach(function (doc) {
-            admin.auth().setCustomUserClaims(uid, { cfId: doc.id, status: 'requested' })
-              .then(() => {
-                console.log('Set claims for ' + uid);
-              })
-              .catch((error) => {
-                console.log('failed cc!');
-              });
-          });
-        })
-        .catch((error) => {
-          console.log("Err err err " + error);
-        });
-    })
-    .catch((error) => {
-      console.log("Err err err err " + error);
-    });
-});
+// uids.forEach((uid) => {
+//   admin.auth().getUser(uid)
+//     .then((user) => {
+//       admin.firestore().collection('communityFoundations').where('personal_email', '==', user.email)
+//         .get()
+//         .then(function (querySnapshot) {
+//           querySnapshot.forEach(function (doc) {
+//             admin.auth().setCustomUserClaims(uid, { cfId: doc.id, status: 'requested' })
+//               .then(() => {
+//                 console.log('Set claims for ' + uid);
+//               })
+//               .catch((error) => {
+//                 console.log('failed cc!');
+//               });
+//           });
+//         })
+//         .catch((error) => {
+//           console.log("Err err err " + error);
+//         });
+//     })
+//     .catch((error) => {
+//       console.log("Err err err err " + error);
+//     });
+// });
 
 //------------------------------------------------------------
 //Script to delete accounts
 //Add the uids into this array and then run this file
 //------------------------------------------------------------
 
-const uidsDelete = [];
+// const uidsDelete = [];
 
-uidsDelete.forEach((uid) => {
-  admin.auth().deleteUser(uid)
-    .then(function () {
-      console.log('Successfully deleted user');
-    })
-    .catch(function (error) {
-      console.log('Error deleting user:', error);
-    });
-});
+// uidsDelete.forEach((uid) => {
+//   admin.auth().deleteUser(uid)
+//     .then(function () {
+//       console.log('Successfully deleted user');
+//     })
+//     .catch(function (error) {
+//       console.log('Error deleting user:', error);
+//     });
+// });
 
 
 
