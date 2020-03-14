9e910fca8c9f4790f64731bfdf9424dfa48fb080
Nathaniel Sprecher <sprechernd1@gcc.edu>
2020-03-14 17:46:40 -0400

    mocked sessionStorage and added relevant tests

--- src/tests/component-tests/TagSearch.test.js
+++ src/tests/component-tests/TagSearch.test.js
@@ -25,16 +25,56 @@ describe('Tag Search basics', () => {
 
   it('Renders correctly', () => {
     const wrap = shallow(<TagSearch />)
+    
     expect(wrap).toMatchSnapshot();
   });
   
 })
 
 describe('All things cache', () => {
+  it('Sets cache appropriately', () =>{
+    const wrap = shallow(<TagSearch {...routeComponentPropsMock} />)
+    const inst = wrap.instance();
+    inst.setCached('blah', 'yada');
+    var data = JSON.parse(sessionStorage['blah']);
+    
+    expect(data.value).toEqual('yada');
+    expect(sessionStorage.length).toBe(1);
+  })
 
-  
-  
+  it('Retrieves cache if cache exists and is recent enough', () =>{
+    sessionStorage.clear();
+    
+    const wrap = shallow(<TagSearch {...routeComponentPropsMock} />)
+    const inst = wrap.instance();
+    inst.setCached('blah', 'yada');
+    const foo = inst.retrieveCached('blah', 100);
+    
+    expect(foo).toBe('yada');
+  })
+
+  it('Returns null if cache does not exist', () =>{
+    sessionStorage.clear();
+    
+    const wrap = shallow(<TagSearch {...routeComponentPropsMock} />)
+    const inst = wrap.instance();
+    const foo = inst.retrieveCached('blah', 100);
+    
+    expect(foo).toBe(null);
+  })
+
+  it('Returns null if cache has expired', () =>{
+    sessionStorage.clear();
+    
+    const wrap = shallow(<TagSearch {...routeComponentPropsMock} />)
+    const inst = wrap.instance();
+    inst.setCached('blah', 'yada');
+    const foo = inst.retrieveCached('blah', 0);
+    
+    expect(foo).toBe(null);
+  })
 })
+
 
 describe('TagSearch method: UpdateSearch', () => {
 
@@ -47,20 +87,26 @@ describe('TagSearch method: UpdateSearch', () => {
   });
 
   it('Gets tags from the database',  () =>{
-
     const firebase = require('firebase');
     var db = firebase.firestore();
+    
     const wrap = shallow(<TagSearch {...routeComponentPropsMock} />)
     const inst = wrap.instance();
+    
     expect(wrap.state('tags')).toEqual([]);
     inst.updateSearch(db, (tags) => expect(tags).toEqual(['tagOne', 'tagTwo']));
-
   });
 
   it('Gets tags from cache',  () =>{
+    sessionStorage.clear();
+    const firebase = require('firebase');
+    var db = firebase.firestore();
 
-    expect('I am not done').toEqual('I am not done');
-
+    const wrap = shallow(<TagSearch {...routeComponentPropsMock} />)
+    const inst = wrap.instance();
+    inst.setCached('tagArray', ['tagOne', 'tagTwo']);
+    
+    inst.updateSearch(db, (tags) => expect(tags).toEqual(['tagOne', 'tagTwo']));
   });
 
 })
@@ -83,21 +129,45 @@ describe('TagSearch method: ComponentWillMount', () => {
 
 describe('TagSearch method: SetDefaultAuto', () => {
 
-  
-  
-})
+  it('Set default if current props',  () =>{
+    const wrap = shallow(<TagSearch {...routeComponentPropsMock} tags={['tagOne', 'tagTwo']}/>)
+    const inst = wrap.instance();
+    
+    expect(inst.setDefaultAuto()).toEqual(['tagOne', 'tagTwo']);
+  });
 
+  it('Set default if incoming tags',  () =>{
+    const wrap = shallow(<TagSearch {...routeComponentPropsMock} />)
+    const inst = wrap.instance();
+    
+    expect(inst.setDefaultAuto('tagOne')).toEqual(['tagOne']);
+  });
 
-
-describe('TagSearch method: HandleClick', () => {
-
-  
+  it('Set default if nothing',  () =>{
+    const wrap = shallow(<TagSearch {...routeComponentPropsMock} />)
+    const inst = wrap.instance();
+    
+    expect(inst.setDefaultAuto()).toEqual(null);
+  });
   
 })
 
 describe('TagSearch method: UpdateParent', () => {
 
-  
+  it('Passes tags and freetext up to parent',  () =>{
+    const wrap = shallow(<TagSearch {...routeComponentPropsMock} 
+      parentCallback={(ret) => {
+        expect(ret).toEqual(
+          { tags: ['tagOne', 'tagTwo'], freeText: ['freeOne', 'freeTwo']  }
+        )
+      }  
+    }/>)
+    const inst = wrap.instance();
+    
+    inst.setState({ activeTags: ['tagOne', 'tagTwo'], activeTextSearch: ['freeOne', 'freeTwo']  }, () => { 
+      inst.updateParent();
+    });
+  });
   
 })
 
