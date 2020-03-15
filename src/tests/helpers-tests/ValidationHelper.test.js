24d7eef17622f6dc09b191d81ebf38fa251d5576
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2020-03-15 01:54:01 -0400

    less stupid

--- src/tests/helpers-tests/ValidationHelper.test.js
+++ src/tests/helpers-tests/ValidationHelper.test.js
@@ -12,45 +12,59 @@ describe('Validate Passwords', () => {
  
   describe('Validate Title', () => {
     it('empty title', () => {
-      expect(validateField({name:'title', value: ''})).toBe('*Please enter a grant title.');
+      expect(validateField('title', '')).toBe('*Please enter a grant title.');
     });
 
     it('mega length too long title', () => {
-        expect(validateField({name:'title', title: "abavavaavavadavacabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcc"})).toBe('*Grant title must be less than 60 characters.');
+        expect(validateField('title', "abavavaavavadavacabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcc")).toBe('*Grant title must be less than 60 characters.');
       }); 
     
     it('good title', () => {
-      expect(validateField({name:'title', value: {title: "Good Title"}})).toBe('');
+      expect(validateField('title', "Good Title")).toBe('');
     }); 
   })
 
   describe('Validate Description', () => {
     it('empty desc', () => {
-      expect(validateField({name:'desc', value: ''})).toBe('*Please enter a grant description.');
+      expect(validateField('desc', '')).toBe('*Please enter a grant description.');
     });
     
     it('good desc', () => {
-      expect(validateField({name:'desc', value: "Good Desc"})).toBe('');
+      expect(validateField('desc', "Good Desc")).toBe('');
     }); 
   })
 
   describe('Validate nonProfit name', () => {
     it('empty name', () => {
-      expect(validateField({name:'nonprofit_name', value: ''})).toBe('*Please select a nonprofit or add a new one.');
+      expect(validateField('nonprofit_name', '')).toBe('*Please select a nonprofit or add a new one.');
     });
     
     it('good name', () => {
-      expect(validateField({name:'nonprofit_name', value: "Good name"})).toBe('');
+      expect(validateField('nonprofit_name', "Good name")).toBe('');
     }); 
   })
 
   describe('Validate address', () => {
-    it('empty name', () => {
-      expect(validateField({name:'nonprofit_name', value: ''})).toBe('*Please select a nonprofit or add a new one.');
+    it('empty address', () => {
+      expect(validateField('address', '')).toBe('*Please select a grant location.');
     });
     
-    it('good name', () => {
-      expect(validateField({name:'nonprofit_name', value: "Good name"})).toBe('');
+    it('good address', () => {
+      expect(validateField('address', "Good address")).toBe('');
+    }); 
+  })
+
+  describe('Validate date deadline', () => {
+    it('empty date', () => {
+      expect(validateField('date_deadline', '')).toBe('*Please select a grant deadline.');
+    });
+
+    it('old date', () => {
+        expect(validateField('date_deadline', (new Date() - 100)/1000 )).toBe('*Please select a grant deadline.');
+      });
+    
+    it('good date', () => {
+      expect(validateField('date_deadline', "Good date")).toBe('');
     }); 
   })
  
