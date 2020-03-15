d13413d1e264adbdef552a1e593ede93970be801
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2020-03-15 14:25:01 -0400

    basic test

---
+++ src/tests/component-tests/Routes.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import { Routes } from '../../components/Routes.js';
+import DDashboard from '../../screens/DDashboard.js';
+import DGive from '../../screens/DGive.js';
+import Grant from '../../screens/Grant.js';
+import FAccountRequest from '../../screens/FAccountRequest.js';
+import FEditGrant from '../../screens/FEditGrant.js';
+import FDashboard from '../../screens/FDashboard.js';
+import FRequestSent from '../../screens/FRequestSent.js';
+import FSettings from '../../screens/FSettings.js';
+import LInputNewPassword from '../../screens/LInputNewPassword.js';
+import SignIn from '../../screens/SignIn.js';
+import SignUp from '../../screens/SignUp.js';
+import ForgotPassword from '../../screens/ForgotPassword.js';
+import MetaAdmin from '../../screens/MetaAdmin.js';
+import FStripeSetup from '../../screens/FStripeSetup.js';
+
+jest.mock('firebase/app');
+
+test('invalid path should not go to any page', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={[ '/random' ]}>
+      <Routes/>
+    </MemoryRouter>
+  );
+  expect(wrapper.find(DDashboard)).toHaveLength(0);
+  expect(wrapper.find(DGive)).toHaveLength(0);
+  expect(wrapper.find(Grant)).toHaveLength(0);
+  expect(wrapper.find(FAccountRequest)).toHaveLength(0);
+  expect(wrapper.find(FEditGrant)).toHaveLength(0);
+  expect(wrapper.find(FDashboard)).toHaveLength(0);
+  expect(wrapper.find(FRequestSent)).toHaveLength(0);
+  expect(wrapper.find(FSettings)).toHaveLength(0);
+  expect(wrapper.find(LInputNewPassword)).toHaveLength(0);
+  expect(wrapper.find(SignIn)).toHaveLength(0);
+  expect(wrapper.find(SignUp)).toHaveLength(0);
+  expect(wrapper.find(ForgotPassword)).toHaveLength(0);
+  expect(wrapper.find(MetaAdmin)).toHaveLength(0);
+  expect(wrapper.find(FStripeSetup)).toHaveLength(0);
+});
+
+test('valid DDashboard path', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={[ '/' ]}>
+      <Routes/>
+    </MemoryRouter>
+  );
+  expect(wrapper.find(DDashboard)).toHaveLength(1);
+  expect(wrapper.find(DGive)).toHaveLength(0);
+  expect(wrapper.find(Grant)).toHaveLength(0);
+  expect(wrapper.find(FAccountRequest)).toHaveLength(0);
+  expect(wrapper.find(FEditGrant)).toHaveLength(0);
+  expect(wrapper.find(FDashboard)).toHaveLength(0);
+  expect(wrapper.find(FRequestSent)).toHaveLength(0);
+  expect(wrapper.find(FSettings)).toHaveLength(0);
+  expect(wrapper.find(LInputNewPassword)).toHaveLength(0);
+  expect(wrapper.find(SignIn)).toHaveLength(0);
+  expect(wrapper.find(SignUp)).toHaveLength(0);
+  expect(wrapper.find(ForgotPassword)).toHaveLength(0);
+  expect(wrapper.find(MetaAdmin)).toHaveLength(0);
+  expect(wrapper.find(FStripeSetup)).toHaveLength(0);
+});
