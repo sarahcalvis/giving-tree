7886127f51e39dcaae066a9df738e25b57db324a
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2020-03-21 20:02:39 -0400

    getting closer

--- src/tests/screen-tests/SignIn.test.js
+++ src/tests/screen-tests/SignIn.test.js
@@ -38,4 +38,86 @@ describe('SignIn with no props', () => {
     });
   });
 
+  it('should have a password field', () => {
+    expect(container.find('#password').at(0).length).toEqual(1);
+  });
+
+  it('should have proper props for password field', () => {
+    expect(container.find('#password').at(0).props()).toEqual({
+      variant: "outlined",
+      margin: "normal",
+      required: true,
+      fullWidth: true,
+      name: "password",
+      onChange: expect.any(Function),
+      type: "password",
+      label: "Password",
+      id: "password",
+      value: "", 
+    });
+  });
+
+  it('should have a submit button', () => {
+    expect(container.find('#submit-button').at(0).length).toEqual(1);
+  });
+
+  it('should have proper props for the submit button', () => {
+    expect(container.find('#submit-button').at(0).props()).toEqual({
+      variant: "contained",
+      children: "Sign In",
+      className: "SignIn-submit-4",
+      color: "primary",
+      disabled: true,
+      fullWidth: true,
+      type: "submit",
+      id: "submit-button",
+    });
+  });
+
 });
+
+describe('SignIn with props', () => {
+    const context = {};
+    const container = mount(
+      <StaticRouter location="someLocation" context={context}>
+          <SignIn/>
+      </StaticRouter>);
+
+    it('should set the email value on change event', () => {
+        container.find('#email').at(0).simulate('change', {
+            target: {
+                name: 'email',
+                value: 'acesmndr@gmail.com',
+            },
+        });
+        expect(container.find('#email').at(0).prop('value')).toEqual(
+            'acesmndr@gmail.com',
+        );
+    });  
+
+    it('should set the password value on change event', () => {
+        container.find('#password').at(0).simulate('change', {
+          target: {
+            name: 'password',
+            value: 'notapassword',
+          },
+        });
+        expect(container.find('#password').at(0).prop('value')).toEqual(
+          'notapassword',
+        );
+      });
+    
+    it('should have proper props for the submit button', () => {
+      expect(container.find('#submit-button').at(0).props()).toEqual({
+        variant: "contained",
+        children: "Sign In",
+        className: "SignIn-submit-4",
+        color: "primary",
+        disabled: true,
+        fullWidth: true,
+        type: "submit",
+        id: "submit-button",
+      });
+    });
+  
+  });
