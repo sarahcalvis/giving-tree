1b403ca99fbc35eddae37652375b8409402b52a8
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2020-03-28 12:29:45 -0400

    removed extra files and got some more testing % ... woot woot

--- src/tests/screen-tests/SignUp.test.js
+++ src/tests/screen-tests/SignUp.test.js
@@ -44,6 +44,7 @@ describe('SignUp with no props', () => {
 
   it('should have proper props for passwordOne field', () => {
     expect(container.find('#passwordOne').at(0).props()).toEqual({
+      error: false,
       variant: "outlined",
       margin: "normal",
       required: true,
@@ -52,7 +53,8 @@ describe('SignUp with no props', () => {
       onChange: expect.any(Function),
       type: "password",
       label: "Password",
-      id: "password",
+      id: "passwordOne",
+      helperText: "",
       value: "", 
     });
   });
@@ -98,12 +100,84 @@ describe('SignUp with no props', () => {
 });
 
 describe('SignUp with props', () => {
-    const context = {};
-    const container = mount(
+  let context, container;
+
+  beforeEach(() => {
+    context = {};
+    container = mount(
       <StaticRouter location="someLocation" context={context}>
-          <SignUp/>
+        <SignUp />
       </StaticRouter>);
-    
+  });
+
+  it('should set the email value on change event', () => {
+    container.find('#email').at(0).props().onChange({
+      target: {
+        name: 'email',
+        value: 'acesmndr@gmail.com',
+      },
+    });
+    expect(container.find('#email').at(0).length).toEqual(1);
+    expect(container.find('SignUpFormBase').state()).toEqual({
+      "email": 'acesmndr@gmail.com',
+      "errors": {
+        "email": "",
+        "passwordOne": "",
+        "passwordTwo": "",
+        "submit": "",
+      },
+      "isValid": false,
+      "passwordOne": '',
+      "passwordTwo": ''
+    });
+  });
+
+  it('should set the password value on change event', () => {
+    container.find('#passwordOne').at(0).props().onChange({
+      target: {
+        name: 'passwordOne',
+        value: 'notapassword',
+      },
+    });
+    expect(container.find('#passwordOne').at(0).length).toEqual(1);
+    expect(container.find('SignUpFormBase').state()).toEqual({
+      "email": "",
+      "errors": {
+        "email": "",
+        "passwordOne": "*Password fails the following requirements: a number, an uppercase letter, a special character (e.g. !@#$%^&*)",
+        "passwordTwo": "",
+        "submit": "",
+      },
+      "isValid": false,
+      "passwordOne": 'notapassword',
+      "passwordTwo": ''
+    });
+
+  });
+
+  it('should set the password check value on change event', () => {
+    container.find('#passwordTwo').at(0).props().onChange({
+      target: {
+        name: 'passwordTwo',
+        value: 'notapassword',
+      },
+    });
+    expect(container.find('#passwordTwo').at(0).length).toEqual(1);
+    expect(container.find('SignUpFormBase').state()).toEqual({
+      "email": "",
+      "errors": {
+        "email": "",
+        "passwordOne": "",
+        "passwordTwo": "*Passwords do not match.",
+        "submit": "",
+      },
+      "isValid": false,
+      "passwordOne": '',
+      "passwordTwo": 'notapassword'
+    });
+
+  });
+
     it('should have proper props for the submit button', () => {
       expect(container.find('#submit-button').at(0).props()).toEqual({
         variant: "contained",
@@ -117,4 +191,67 @@ describe('SignUp with props', () => {
       });
     });
   
+  });
+
+  describe('SignUp with props', () => {
+    let context, container;
+  
+    beforeEach(() => {
+      context = {};
+      container = mount(
+        <StaticRouter location="someLocation" context={context}>
+          <SignUp />
+        </StaticRouter>);
+    });
+  
+    it('should allow submit when given correct input', () => {
+      container.find('#email').at(0).props().onChange({
+        target: {
+          name: 'email',
+          value: 'acesmndr@gmail.com',
+        },
+      });
+      expect(container.find('#email').at(0).length).toEqual(1);
+      
+      container.find('#passwordOne').at(0).props().onChange({
+        target: {
+          name: 'passwordOne',
+          value: '1q!Q1q!q',
+        },
+      });
+      expect(container.find('#passwordOne').at(0).length).toEqual(1);
+      
+      container.find('#passwordTwo').at(0).props().onChange({
+        target: {
+          name: 'passwordTwo',
+          value: '1q!Q1q!q',
+        },
+      });
+      expect(container.find('#passwordTwo').at(0).length).toEqual(1);
+      
+      expect(container.find('SignUpFormBase').state()).toEqual({
+        "email": "acesmndr@gmail.com",
+        "errors": {
+          "email": "",
+          "passwordOne": "",
+          "passwordTwo": "",
+          "submit": "",
+        },
+        "isValid": true,
+        "passwordOne": '1q!Q1q!q',
+        "passwordTwo": '1q!Q1q!q'
+      });
+      
+      expect(container.find('#submit-button').at(0).props()).toEqual({
+        variant: "contained",
+        children: "Sign Up",
+        className: "SignUp-submit-4",
+        color: "primary",
+        disabled: true,
+        fullWidth: true,
+        type: "submit",
+        id: "submit-button",
+      });
+    });
+    
   });
