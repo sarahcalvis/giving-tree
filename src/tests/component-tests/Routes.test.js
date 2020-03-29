ad83c9378a789d93ee1a2d5bd0bfd11556138c61
Calvis <sarahcalvis@gmail.com>
2020-03-29 09:45:36 -0400

    Fixed routes tests so they pass

--- src/tests/component-tests/Routes.test.js
+++ src/tests/component-tests/Routes.test.js
@@ -7,33 +7,32 @@ import { createMemoryHistory } from "history";
 
 import Routes from '../../components/Routes.js';
 import DDashboard from '../../screens/DDashboard.js';
-import {DGive} from '../../screens/DGive.js';
-import {Grant} from '../../screens/Grant.js';
-import {FAccountRequest} from '../../screens/FAccountRequest.js';
-import {FEditGrant} from '../../screens/FEditGrant.js';
-import {FDashboard} from '../../screens/FDashboard.js';
-import {FRequestSent} from '../../screens/FRequestSent.js';
-import {FSettings} from '../../screens/FSettings.js';
-import {LInputNewPassword} from '../../screens/LInputNewPassword.js';
-import {SignIn} from '../../screens/SignIn.js';
-import {SignUp} from '../../screens/SignUp.js';
-import {ForgotPassword} from '../../screens/ForgotPassword.js';
-import {MetaAdmin} from '../../screens/MetaAdmin.js';
-import {FStripeSetup} from '../../screens/FStripeSetup.js';
+import { DGive } from '../../screens/DGive.js';
+import { Grant } from '../../screens/Grant.js';
+import { FAccountRequest } from '../../screens/FAccountRequest.js';
+import { FEditGrant } from '../../screens/FEditGrant.js';
+import { FDashboard } from '../../screens/FDashboard.js';
+import { FRequestSent } from '../../screens/FRequestSent.js';
+import { FSettings } from '../../screens/FSettings.js';
+import { SignIn } from '../../screens/SignIn.js';
+import { SignUp } from '../../screens/SignUp.js';
+import { ForgotPassword } from '../../screens/ForgotPassword.js';
+import { MetaAdmin } from '../../screens/MetaAdmin.js';
+import { FStripeSetup } from '../../screens/FStripeSetup.js';
 
 jest.mock('firebase/app');
 jest.mock("react-router-dom/BrowserRouter", () => {
     return ({ children }) => <div>{children}</div>;
-  });
+});
 
 describe('Component: Routes', () => {
     var enzyme = require('enzyme');
     enzyme.configure({ adapter: new Adapter() });
-     
+
     it('invalid path should not go to any page', () => {
         const wrapper = enzyme.mount(
-            <MemoryRouter initialEntries={[ '/random' ]}>
-                <Routes />
+            <MemoryRouter initialEntries={['/random']}>
+                {Routes}
             </MemoryRouter>
         );
         console.log(wrapper.find('Route').at(0).debug());
@@ -45,7 +44,6 @@ describe('Component: Routes', () => {
         expect(wrapper.find('FDashboard')).toHaveLength(0);
         expect(wrapper.find('FRequestSent')).toHaveLength(0);
         expect(wrapper.find('FSettings')).toHaveLength(0);
-        expect(wrapper.find('LInputNewPassword')).toHaveLength(0);
         expect(wrapper.find('SignIn')).toHaveLength(0);
         expect(wrapper.find('SignUp')).toHaveLength(0);
         expect(wrapper.find('ForgotPassword')).toHaveLength(0);
@@ -55,13 +53,13 @@ describe('Component: Routes', () => {
 
     it('valid DDashboard path', () => {
         const wrapper = enzyme.mount(
-        <MemoryRouter initialEntries={['/']}>
-            {Routes}
-        </MemoryRouter>
-    );
-    //expect(wrapper.containsMatchingElement(DDashboard)).toBeTruthy();
-      
-    expect(wrapper.find('DDashboard')).toHaveLength(0);
+            <MemoryRouter initialEntries={['/']}>
+                {Routes}
+            </MemoryRouter>
+        );
+        //expect(wrapper.containsMatchingElement(DDashboard)).toBeTruthy();
+
+        expect(wrapper.find(DDashboard)).toHaveLength(0);
     });
 
 })
