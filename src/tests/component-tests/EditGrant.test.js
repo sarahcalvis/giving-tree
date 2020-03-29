8635f78225c01d167c99470d55b0082d9ea2db40
Calvis <sarahcalvis@gmail.com>
2020-03-29 10:50:58 -0400

    Testing Grant Description

--- src/tests/component-tests/EditGrant.test.js
+++ src/tests/component-tests/EditGrant.test.js
@@ -2,15 +2,8 @@ import '@testing-library/jest-dom'
 import React from 'react'
 import { render, fireEvent, screen } from '@testing-library/react'
 import EditGrant from '../../components/EditGrant'
-const { mockFirebase } = require('firestore-jest-mock');
-import { shallow, mount } from 'enzyme';
 
-describe('Edit Grant basics', () => {
-
-  it('Renders correctly', () => {
-    // const wrap = shallow(<EditGrant />)
-    
-    // expect(wrap).toMatchSnapshot();
-  });
-  
-})
+test('Edit Grant Basics', () => {
+  // render(<EditGrant />)
+  // expect(screen).toMatchSnapshot();
+});
