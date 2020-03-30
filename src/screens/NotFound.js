c1efd78cf2caf3b687282bdc27e3fb8270a2bf35
Calvis <sarahcalvis@gmail.com>
2020-03-30 08:23:12 -0400

    Testing NotFound.js

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   message: {
@@ -17,7 +17,7 @@ function FRequestDenied() {
     <Container maxWidth="sm" component="main" className={classes.message}>
       <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
         4040404 Not Found
-        </Typography>
+      </Typography>
     </Container>
   );
 }
