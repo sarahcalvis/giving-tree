1a89c5439dc00046cc7473b0f4f044e31eb7fca1
Calvis <sarahcalvis@gmail.com>
2020-03-31 10:15:37 -0400

    Testing SmallGrantCard

--- src/tests/component-tests/SmallGrantCard.test.js
+++ src/tests/component-tests/SmallGrantCard.test.js
@@ -1,8 +1,11 @@
 import '@testing-library/jest-dom'
 import React from 'react'
 import { shallow } from 'enzyme';
+import FirestoreMock from '../firestore.mock'
 
 import SmallGrantCard from '../../components/SmallGrantCard'
+
+const mockFirestore = new FirestoreMock();
 
 const routeComponentPropsMock = {
   history: {},
@@ -10,12 +13,37 @@ const routeComponentPropsMock = {
   match: {}
 }
 
+
+jest.mock('firebase/app', () => ({
+  __esModule: true,
+  initializeApp: () => { },
+  apps: {
+    length: true,
+  },
+  app: () => {
+    return {
+      auth: () => { },
+      firestore: () => { return mockFirestore },
+      storage: () => {
+        return {
+          get: jest.fn(),
+          ref: () => {
+            return {
+              child: jest.fn(),
+            }
+          }
+        }
+      },
+    }
+  },
+}));
+
 describe('Small Grant Card basics', () => {
 
   it('Renders correctly', () => {
-    // const wrap = shallow(<SmallGrantCard  {...routeComponentPropsMock} />)
+    const wrap = shallow(<SmallGrantCard  {...routeComponentPropsMock} />)
 
-    // expect(wrap).toMatchSnapshot();
+    expect(wrap).toMatchSnapshot();
   });
 
 })
