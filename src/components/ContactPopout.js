2ae9879924f8fe414822d64d844899b46a798cfe
Calvis <sarahcalvis@gmail.com>
2020-04-13 11:52:38 -0400

    Made contact popout great again

--- src/components/ContactPopout.js
+++ src/components/ContactPopout.js
@@ -1,95 +1,71 @@
 import React from 'react';
 
 import Link from '@material-ui/core/Link';
-import Popover from '@material-ui/core/Popover';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import ArrowDropDown from '@material-ui/icons/ArrowDropDown';
+import ArrowRight from '@material-ui/icons/ArrowRight';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   a: {
     cursor: 'pointer',
   },
-  popover: {
-    maxWidth: 500,
-  },
-  typographyTop: {
-    padding: theme.spacing(2),
-  },
-  typographyBottom: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
-  }
 }));
 
 export default function ContactPopout(props) {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [icon, setIcon] = React.useState(<ArrowRight />);
 
   const handleClick = event => {
-    setAnchorEl(event.currentTarget);
+    if (open) {
+      setOpen(false)
+      setIcon(<ArrowRight />)
+    } else {
+      setOpen(true);
+      setIcon(<ArrowDropDown />)
+    }
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
   const id = open ? 'Contact information' : undefined;
 
   return (
     <div>
       <a className={classes.a} aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
-        <Button variant='outlined' color='primary'>
+        <Button endIcon={icon} >
           Contact
         </Button>
       </a>
-      <Popover
-        className={classes.popover}
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-      >
-        <Typography
-          className={classes.typographyTop}
-          variant="body2">
-          Please contact <Link
-            textDecoration='none'
-            target='_blank'
-            rel='noopener noreferrer'
-            href={props.cfData.url}>
-            {props.cfData.name}
-          </Link> with any concerns at <Link href={'tel:' + props.cfData.phone}>
-            {props.cfData.public_phone}
-          </Link> or <Link href={'mailto:' + props.cfData.public_email}>
-            {props.cfData.email}
-          </Link>
-        </Typography>
-        <Typography
-          className={classes.typographyBottom}
-          variant="body2">
-          <Link
-            textDecoration='none'
-            target='_blank'
-            rel='noopener noreferrer'
-            href={props.nonprofitData.url}>{props.nonprofitData.name}
-          </Link> can be reached at <Link href={'tel:' + props.nonprofitData.phone}>
-            {props.nonprofitData.phone}
-          </Link> or <Link href={'mailto:' + props.nonprofitData.email}>
-            {props.nonprofitData.email}
-          </Link>
-        </Typography>
-      </Popover>
+      {open &&
+        <div>
+          <Typography variant="body2">
+            Please contact <Link
+              textDecoration='none'
+              target='_blank'
+              rel='noopener noreferrer'
+              href={props.cfData.url}>
+              {props.cfData.name}
+            </Link> with any concerns at <Link href={'tel:' + props.cfData.phone}>
+              {props.cfData.public_phone}
+            </Link> or <Link href={'mailto:' + props.cfData.public_email}>
+              {props.cfData.email}
+            </Link>
+          </Typography>
+          <Typography variant="body2">
+            <Link
+              textDecoration='none'
+              target='_blank'
+              rel='noopener noreferrer'
+              href={props.nonprofitData.url}>{props.nonprofitData.name}
+            </Link> can be reached at <Link href={'tel:' + props.nonprofitData.phone}>
+              {props.nonprofitData.phone}
+            </Link> or <Link href={'mailto:' + props.nonprofitData.email}>
+              {props.nonprofitData.email}
+            </Link>
+          </Typography>
+        </div>
+      }
     </div>
   );
 }
