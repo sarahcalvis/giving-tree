2ba630d149bc71c27004edbb95bb516a9e66b127
Stahl4121 <3391556+Stahl4121@users.noreply.github.com>
2020-04-21 18:03:02 -0400

    Fixed useEffect infinite cycle

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from '@material-ui/core/Grid';
 
@@ -23,92 +23,118 @@ const useStyles = makeStyles(theme => ({
 
 export default function Search(props) {
   const classes = useStyles();
-  
+
   //State Variables
-  const [searchDocs, setSearchDocs] = useState([{ dist: -1, data: {}}]);
-  const [searchResults, setSearchResults] = useState([{ dist: -1, data: {}}]);
-  const [tagSearchResults, setTagSearchResults] = useState({tags: [], freeText: []});
+  const [searchDocs, setSearchDocs] = useState([{ dist: -1, data: {} }]);
+  const [searchResults, setSearchResults] = useState([{ dist: -1, data: {} }]);
+  const [tagSearchResults, setTagSearchResults] = useState({ tags: [], freeText: [] });
   const [radius, setRadius] = useState(-1);
-  const [location, setLocation] = useState({address: '', lat: -1000, long: -1000});
+  const [location, setLocation] = useState({ address: '', lat: -1000, long: -1000 });
   const [sortBy, setSortBy] = useState('');
   const [updateDistCount, setUpdateDistCount] = useState(0);
 
   //Destructure props
   const props_docs = props.docs;
-  const props_parentCallback = props.parentCallback;
+  const memoizedParentCallback = useCallback(props.parentCallback, [props.parentCallback]);
+
+  //Heckin Hacky Code
+  const isMountedFilter = useRef(0);
+  const isMountedSort = useRef(false);
+  const isMountedDist = useRef(false);
 
   useEffect(() => {
     console.log(1);
     let newSearchDocs = [];
     props_docs.forEach((doc) => {
-      newSearchDocs.push({ dist: -1, data: doc});
+      newSearchDocs.push({ dist: -1, data: doc });
     });
     setSearchDocs(newSearchDocs);
   }, [props_docs]);
 
   //Filters the results when search component values change
   useEffect(() => {
-    console.log(2);
-    let results = searchDocs.filter((searchDoc) => {
-      const {tags, freeText} = tagSearchResults;
-      const dat = searchDoc.data;
-      const dist = searchDoc.dist;
+    if (isMountedFilter.current >= 2) {
+      console.log(2);
+      let results = searchDocs.filter((searchDoc) => {
+        const { tags, freeText } = tagSearchResults;
+        const dat = searchDoc.data;
+        const dist = searchDoc.dist;
 
-      //Check that doc includes all tags in the search
-      if (tags.length !== 0){
-        const lcTags = dat.tags.map(t => t.toLowerCase());
-        if (!tags.every(tag => (lcTags).includes(tag.toLowerCase()))) return false;
-      } 
-      //Check that doc includes all free text in the search
-      if (freeText.length !== 0) {
-        const docText = (dat.desc + dat.title + dat.nonprofitName + dat.cfName).toLowerCase();
-        if(!freeText.every(el => (docText).includes(el.toLowerCase()))) return false;
-      }
-      //Check that doc is within the radius specified
-      if(radius !== -1 && dist > radius) return false;
-      
-    });
+        //Check that doc includes all tags in the search
+        if (tags.length !== 0) {
+          const lcTags = dat.tags.map(t => t.toLowerCase());
+          if (!tags.every(tag => (lcTags).includes(tag.toLowerCase()))) return false;
+        }
+        //Check that doc includes all free text in the search
+        if (freeText.length !== 0) {
+          const docText = (dat.desc + dat.title + dat.nonprofitName + dat.cfName).toLowerCase();
+          if (!freeText.every(el => (docText).includes(el.toLowerCase()))) return false;
+        }
+        //Check that doc is within the radius specified
+        if (radius !== -1 && dist > radius) return false;
 
-    setSearchResults(results);
+        return true;
+      });
 
-  }, [props_parentCallback, searchDocs, tagSearchResults, updateDistCount, radius]);
+      setSearchResults(results);
+    }
+    else {
+      isMountedFilter.current += 1;
+    }
+  }, [searchDocs, tagSearchResults, updateDistCount, radius]);
 
   //Sorts and returns the search results when either they or sortBy changes
   useEffect(() => {
-    console.log(3);
-    let sortedResults = searchResults.slice();
-    sortedResults.sort((a, b) => { return (a.data.dist - b.data.dist)});
+    if (isMountedSort.current) {
 
-    switch(sortBy){
-      case "deadline":
-        sortedResults.sort((a, b) => { return (a.data.dateDeadline.seconds - b.data.dateDeadline.seconds)});
-        break;
-      case "posting":
-        sortedResults.sort((a, b) => { return (a.data.datePosted.seconds - b.data.datePosted.seconds)});
-        break;
-      case "goalD":
-        sortedResults.sort((a, b) => { return ((b.data.moneyRaised / b.data.goalAmt) - (a.data.moneyRaised / a.data.goalAmt))});
-        break;
-      case "goalI":
-        sortedResults.sort((a, b) => { return ((a.data.moneyRaised / a.data.goalAmt) - (b.data.moneyRaised / b.data.goalAmt))});
-        break;
-      case "sizeI":
-        sortedResults.sort((a, b) => { return (a.data.goalAmt - b.data.goalAmt)});
-        break;
-      case "sizeD":
-        sortedResults.sort((a, b) => { return (b.data.goalAmt - a.data.goalAmt)});
-        break;
-      default:
+      console.log(3);
+      let sortedResults = searchResults.slice();
+      sortedResults.sort((a, b) => { return (a.data.dist - b.data.dist) });
+
+      switch (sortBy) {
+        case "deadline":
+          sortedResults.sort((a, b) => { return (a.data.dateDeadline.seconds - b.data.dateDeadline.seconds) });
+          break;
+        case "posting":
+          sortedResults.sort((a, b) => { return (a.data.datePosted.seconds - b.data.datePosted.seconds) });
+          break;
+        case "goalD":
+          sortedResults.sort((a, b) => { return ((b.data.moneyRaised / b.data.goalAmt) - (a.data.moneyRaised / a.data.goalAmt)) });
+          break;
+        case "goalI":
+          sortedResults.sort((a, b) => { return ((a.data.moneyRaised / a.data.goalAmt) - (b.data.moneyRaised / b.data.goalAmt)) });
+          break;
+        case "sizeI":
+          sortedResults.sort((a, b) => { return (a.data.goalAmt - b.data.goalAmt) });
+          break;
+        case "sizeD":
+          sortedResults.sort((a, b) => { return (b.data.goalAmt - a.data.goalAmt) });
+          break;
+        default:
+      }
+
+      memoizedParentCallback(sortedResults);
     }
-
-    props_parentCallback(sortedResults);
-  }, [props_parentCallback, sortBy, searchResults]);
+    else {
+      isMountedSort.current = true;
+    }
+  }, [sortBy, searchResults]);
 
   //Updates the distances of grants when location changes
   useEffect(() => {
-    console.log(4);
-    searchDocs.map((searchDoc) => searchDoc.dist = helper.calcDistance(location.lat, location.long, searchDoc.data.lat, searchDoc.data.long));
-    setUpdateDistCount(u => u+1); //Bad practice
+    if (isMountedDist.current) {
+      console.log(4);
+      searchDocs.forEach((searchDoc) => {
+        if (location.address !== '') {
+          searchDoc.dist = helper.calcDistance(location.lat, location.long, searchDoc.data.lat, searchDoc.data.long);
+        }
+        else searchDoc.dist = -1;
+      });
+      setUpdateDistCount(u => u + 1); //Bad practice
+    }
+    else {
+      isMountedDist.current = true;
+    }
   }, [location, searchDocs]);
 
 
